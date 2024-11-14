198e440e8168ce444aded73069e6fc122cd2ea28
pablogrp <alu0101496139@ull.edu.es>
2024-11-14 11:00:22 +0000

    New commit

---
+++ my-app/my-app/src/main/java/es/ull/app/App.java
@@ -0,0 +1,10 @@
+package es.ull.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! This is a change");
+    }
+}
