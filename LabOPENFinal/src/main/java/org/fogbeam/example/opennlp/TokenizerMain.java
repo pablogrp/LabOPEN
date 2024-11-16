af9aab3f4a4a637ce5927caa331e4c2e8fb80f32
pablogrp <alu0101496139@ull.edu.es>
2024-11-16 17:42:20 +0000

    Reestructuración clase TokenizerMain

--- LabOPENFinal/src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
+++ LabOPENFinal/src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
@@ -13,48 +13,33 @@ import opennlp.tools.tokenize.TokenizerModel;
 
 public class TokenizerMain
 {
-	public static void main( String[] args ) throws Exception
-	{
-		
+	public static void main( String[] args ) throws Exception {
 		// the provided model
 		// InputStream modelIn = new FileInputStream( "models/en-token.bin" );
-
-		
 		// the model we trained
 		InputStream modelIn = new FileInputStream( "models/en-token.model" );
-		
-		try
-		{
+		try	{
 			TokenizerModel model = new TokenizerModel( modelIn );
-		
 			Tokenizer tokenizer = new TokenizerME(model);
-			
 				/* note what happens with the "three depending on which model you use */
-			String[] tokens = tokenizer.tokenize
-					(  "A ranger journeying with Oglethorpe, founder of the Georgia Colony, " 
+			String[] tokens = tokenizer.tokenize (  "A ranger journeying with Oglethorpe, founder of the Georgia Colony, "
 							+ " mentions \"three Mounts raised by the Indians over three of their Great Kings" 
 							+ " who were killed in the Wars.\"" );
 			
-			for( String token : tokens )
-			{
+			for( String token : tokens ) {
 				System.out.println( token );
 			}
-			
 		}
-		catch( IOException e )
-		{
+		catch( IOException e ) {
 			e.printStackTrace();
 		}
-		finally
-		{
-			if( modelIn != null )
-			{
-				try
-				{
+		finally	{
+			if( modelIn != null ) {
+				try	{
 					modelIn.close();
 				}
-				catch( IOException e )
-				{
+				catch( IOException e ) {
+					e.printStackTrace();
 				}
 			}
 		}
