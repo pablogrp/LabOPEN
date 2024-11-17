d1946ebff190ed1b00652eced95288ee15a416e2
pablogrp <alu0101496139@ull.edu.es>
2024-11-17 12:50:59 +0000

    Comentado en formato doxygen la clase ParserTrainer

--- LabOPENFinal/src/main/java/org/fogbeam/example/opennlp/training/ParserTrainer.java
+++ LabOPENFinal/src/main/java/org/fogbeam/example/opennlp/training/ParserTrainer.java
@@ -1,7 +1,15 @@
 package org.fogbeam.example.opennlp.training;
 
+/**
+ * @class ParserTrainer
+ * @brief Main class for training a parser model using OpenNLP.
+ */
 public class ParserTrainer{
-
+	/**
+	 * @brief Main method to train the parser model.
+	 * @param args Command line arguments.
+	 * @throws Exception if an error occurs during training.
+	 */
 	public static void main( String[] args ){
 		System.out.println( "done" );
 	}
