fe6cf3d5e56190a60d81d9b43b796785f7fd961a
pablogrp <alu0101496139@ull.edu.es>
2024-11-17 12:43:20 +0000

    Comentado en formato doxygen la clase DocumentClassifierMain

--- LabOPENFinal/src/main/java/org/fogbeam/example/opennlp/DocumentClassifierMain.java
+++ LabOPENFinal/src/main/java/org/fogbeam/example/opennlp/DocumentClassifierMain.java
@@ -1,36 +1,42 @@
 package org.fogbeam.example.opennlp;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import opennlp.tools.doccat.DoccatModel;
 import opennlp.tools.doccat.DocumentCategorizerME;
 
-public class DocumentClassifierMain
-{
+/**
+ * @class DocumentClassifierMain
+ * @brief Main class for classifying documents using OpenNLP.
+ */
+public class DocumentClassifierMain {
 
-	public static void main( String[] args ) throws Exception {
+	/**
+	 * @brief Main method to run the document classifier.
+	 * @param args Command line arguments.
+	 * @throws Exception if an error occurs during document classification.
+	 */
+	public static void main(String[] args) throws Exception {
 		InputStream is = null;
-		try	{
-			is = new FileInputStream( "models/en-doccat.model" );
-		
-			DoccatModel m = new DoccatModel(is);
+		try {
+			is = new FileInputStream("models/en-doccat.model");
+
+			DoccatModel model = new DoccatModel(is);
 			String inputText = "What happens if we have declining bottom-line revenue?";
-			DocumentCategorizerME myCategorizer = new DocumentCategorizerME(m);
-			double[] outcomes = myCategorizer.categorize(inputText);
-			String category = myCategorizer.getBestCategory( outcomes );
-			
-			System.out.println( "Input classified as: " + category );
-		}
-		catch( Exception e ) {
+			DocumentCategorizerME categorizer = new DocumentCategorizerME(model);
+			double[] outcomes = categorizer.categorize(inputText);
+			String category = categorizer.getBestCategory(outcomes);
+
+			System.out.println("Input classified as: " + category);
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally	{
-			if( is != null ) {
+		} finally {
+			if (is != null) {
 				is.close();
 			}
 		}
-		
-		System.out.println( "done" );
+		System.out.println("done");
 	}
 }
