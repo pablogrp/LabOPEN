9ccf39b7424631e86c3a9768f9186314db96765d
pablogrp <alu0101496139@ull.edu.es>
2024-11-17 12:38:14 +0000

    Comentado en formato doxygen la clase PartOfSpeechTaggerMain

--- LabOPENFinal/src/main/java/org/fogbeam/example/opennlp/PartOfSpeechTaggerMain.java
+++ LabOPENFinal/src/main/java/org/fogbeam/example/opennlp/PartOfSpeechTaggerMain.java
@@ -9,23 +9,25 @@ import java.io.InputStream;
 import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSTaggerME;
 
-
+/**
+ * Clase PartOfSpeechTaggerMain
+ */
 public class PartOfSpeechTaggerMain {
+	/**
+	 * Método main
+	 * @param args
+	 */
 	public static void main( String[] args ) {
 		InputStream modelIn = null;
 		try	{
 			// modelIn = new FileInputStream( "models/en-pos.model" );
 			modelIn = new FileInputStream( "models/en-pos-maxent.bin" );
-			
 			POSModel model = new POSModel( modelIn );
 			POSTaggerME tagger = new POSTaggerME(model);
-			
 			String sent[] = new String[]{"Most", "large", "cities", "in", "the", "US", "had",
                     "morning", "and", "afternoon", "newspapers", "."};		  
 			String tags[] = tagger.tag(sent);
-			
 			double probs[] = tagger.probs();
-
 			for( int i = 0; i < sent.length; i++ )	{
 				System.out.println( "Token [" + sent[i] + "] has POS [" + tags[i] + "] with probability = " + probs[i] );
 			}
