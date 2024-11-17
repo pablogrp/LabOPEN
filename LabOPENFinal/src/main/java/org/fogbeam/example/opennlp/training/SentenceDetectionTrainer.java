5af1ae70c23b878a7f057934ddca4400ef7aca36
pablogrp <alu0101496139@ull.edu.es>
2024-11-17 12:47:10 +0000

    Comentado en formato doxygen la clase SentenceDetectionTrainer

--- LabOPENFinal/src/main/java/org/fogbeam/example/opennlp/training/SentenceDetectionTrainer.java
+++ LabOPENFinal/src/main/java/org/fogbeam/example/opennlp/training/SentenceDetectionTrainer.java
@@ -1,8 +1,6 @@
-
 package org.fogbeam.example.opennlp.training;
 
 import java.io.BufferedOutputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
@@ -16,34 +14,39 @@ import opennlp.tools.util.ObjectStream;
 import opennlp.tools.util.PlainTextByLineStream;
 import opennlp.tools.util.TrainingParameters;
 
-
-public class SentenceDetectionTrainer
-{
-	public static void main( String[] args ) throws Exception {
-		Charset charset = Charset.forName("UTF-8");				
-		ObjectStream<String> lineStream = 
-				new PlainTextByLineStream(new FileInputStream("training_data/en-sent.train"), charset);
+/**
+ * @class SentenceDetectionTrainer
+ * @brief Main class for training a sentence detection model using OpenNLP.
+ */
+public class SentenceDetectionTrainer {
+	/**
+	 * @brief Main method to train the sentence detection model.
+	 * @param args Command line arguments.
+	 * @throws Exception if an error occurs during training.
+	 */
+	public static void main(String[] args) throws Exception {
+		Charset charset = Charset.forName("UTF-8");
+		ObjectStream<String> lineStream = new PlainTextByLineStream(new FileInputStream("training_data/en-sent.train"), charset);
 		ObjectStream<SentenceSample> sampleStream = new SentenceSampleStream(lineStream);
 
 		SentenceModel model;
 
 		try {
-		  model = SentenceDetectorME.train("en", sampleStream, true, null, TrainingParameters.defaultParams());
-		}
-		finally {
-		  sampleStream.close();
+			model = SentenceDetectorME.train("en", sampleStream, true, null, TrainingParameters.defaultParams());
+		} finally {
+			sampleStream.close();
 		}
 
 		OutputStream modelOut = null;
-		File modelFile = new File( "models/en-sent.model" );
 		try {
-		  modelOut = new BufferedOutputStream(new FileOutputStream(modelFile));
-		  model.serialize(modelOut);
+			modelOut = new BufferedOutputStream(new FileOutputStream("models/en-sent.model"));
+			model.serialize(modelOut);
 		} finally {
-		  if (modelOut != null) 
-		     modelOut.close();      
-		}		
-		
-		System.out.println( "done" );
+			if (modelOut != null) {
+				modelOut.close();
+			}
+		}
+
+		System.out.println("done");
 	}
 }
